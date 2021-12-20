29d3a496e697886cce6f9aca32ec0bac8e26c162
Liam-gosnell <liamgoz99@gmail.com>
2021-12-20 18:16:46 +0000

    Added button inline styles

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,21 +7,22 @@ import {
 
 function Sidebar() {
     return (
+    <div className='text-gray-500'>
         <div>
-            <button>
+            <button className='flex items-center space-x-2 hover:text-white'>
                 <HomeIcon className="h-5 w-5" />
+                <p>Home</p>
             </button>
-            <button>
+            <button className='flex items-center space-x-2 hover:text-white'>
                 <SearchIcon className="h-5 w-5" />
+                <p>Search</p>
             </button>
-            <button>
+            <button className='flex items-center space-x-2 hover:text-white'>
                 <LibraryIcon className="h-5 w-5" />
+                <p>Your Library</p>
             </button>
-            <button>
-                <PlusCircleIcon className="h-5 w-5" />
-            </button>
-
         </div>
+    </div>
     )
 }
 
